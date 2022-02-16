69e162dcde27ef77f6ffd888b079c82582ba5940
ben1289 <ben_1289@163.com>
2022-02-16 22:55:40 +0800

    refactor: 解决Tailwind覆盖Naive UI样式问题

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import App from './App.vue';
 import router from './router';
 import './assets/styles/index.css';
 
+const meta = document.createElement('meta');
+meta.name = 'naive-ui-style';
+document.head.appendChild(meta);
+
 createApp(App).use(router).mount('#app');
