9fa6a1a7d60d6a90ad34538a72dad49753dbfe4b
ben1289 <ben_1289@163.com>
2022-02-19 23:52:52 +0800

    feat: 新增NaiveProvider组件

---
+++ src/components/NaiveProvider/components/NaiveProviderContent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts" name="NaiveProviderContent">
+import {
+  useLoadingBar,
+  useDialog,
+  useNotification,
+  useMessage,
+} from 'naive-ui';
+
+window.$loadingBar = useLoadingBar();
+window.$dialog = useDialog();
+window.$notification = useNotification();
+window.$message = useMessage();
+</script>
---
+++ src/components/NaiveProvider/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <n-loading-bar-provider>
+    <n-dialog-provider>
+      <n-notification-provider>
+        <n-message-provider>
+          <NaiveProviderContent />
+          <slot />
+        </n-message-provider>
+      </n-notification-provider>
+    </n-dialog-provider>
+  </n-loading-bar-provider>
+</template>
+
+<script setup lang="ts" name="NaiveProvider">
+import {
+  NLoadingBarProvider,
+  NDialogProvider,
+  NNotificationProvider,
+  NMessageProvider,
+} from 'naive-ui';
+import NaiveProviderContent from './components';
+</script>
+
+<style scoped></style>
