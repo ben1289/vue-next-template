d72ce7b04f8aedc0315259e4cebded5ae7fe1e76
ben1289 <ben_1289@163.com>
2022-02-21 23:24:39 +0800

    refactor: 去除多余alias、去除proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,5 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueSetupExtend()],
-  resolve: {
-    alias: {
-      '@': '/src',
-      assets: '/src/assets',
-      components: '/src/components',
-      views: '/src/views',
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:2000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  resolve: { alias: { '@': '/src' } },
 });
