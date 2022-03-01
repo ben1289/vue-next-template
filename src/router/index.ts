17ff44623f1180923ab9b0319b0a425345724666
ben1289 <ben_1289@163.com>
2022-03-02 00:19:31 +0800

    refactor: 增加路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,23 @@
+import type { App } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from './routes';
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(),
   routes,
 });
 
-export default router;
+export async function setupRouter(app: App) {
+  app.use(router);
+
+  router.beforeEach((to, from) => {
+    window.$loadingBar.start();
+    console.log(to, from);
+  });
+  router.afterEach((to, from) => {
+    console.log(to, from);
+    window.$loadingBar.finish();
+  });
+
+  await router.isReady();
+}
