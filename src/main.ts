e6e42a9c3b425c9c9c4565f1995338f609caaed5
ben1289 <ben_1289@163.com>
2022-03-02 00:24:51 +0800

    refactor: 初始化挂载Store、Naive UI、Router

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import { setupStore } from './store';
+import { setupNaiveUI } from './plugins';
+import { setupRouter } from './router';
 import './assets/styles/index.css';
 
+// 解决Tailwind覆盖Naive UI样式问题
 const meta = document.createElement('meta');
 meta.name = 'naive-ui-style';
 document.head.appendChild(meta);
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+// 挂载pinia
+setupStore(app);
+
+// 按需引入Naive UI
+setupNaiveUI(app);
+
+// Router准备就绪后挂载app
+setupRouter(app).then(() => {
+  app.mount('#app');
+});
