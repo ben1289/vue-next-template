7609f27e82d9d42a3dbe773973096d7d1f1ac2c4
ben1289 <ben_1289@163.com>
2022-03-01 21:48:56 +0800

    refactor: 增加NConfigProvider、NGlobalStyle

--- src/components/NaiveProvider/index.vue
+++ src/components/NaiveProvider/index.vue
@@ -1,18 +1,23 @@
 <template>
-  <n-loading-bar-provider>
-    <n-dialog-provider>
-      <n-notification-provider>
-        <n-message-provider>
-          <NaiveProviderContent />
-          <slot />
-        </n-message-provider>
-      </n-notification-provider>
-    </n-dialog-provider>
-  </n-loading-bar-provider>
+  <n-config-provider>
+    <n-global-style />
+    <n-loading-bar-provider>
+      <n-dialog-provider>
+        <n-notification-provider>
+          <n-message-provider>
+            <NaiveProviderContent />
+            <slot />
+          </n-message-provider>
+        </n-notification-provider>
+      </n-dialog-provider>
+    </n-loading-bar-provider>
+  </n-config-provider>
 </template>
 
 <script setup lang="ts" name="NaiveProvider">
 import {
+  NConfigProvider,
+  NGlobalStyle,
   NLoadingBarProvider,
   NDialogProvider,
   NNotificationProvider,
